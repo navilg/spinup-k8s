c1640e4c97a797922d81fd34241c63b7cff9e8b2
Navratan Gupta <navilg0409@gmail.com>
2022-01-02 17:07:45 +0530

    Improvements

--- spinup-k8s.sh
+++ spinup-k8s.sh
@@ -14,6 +14,8 @@ if [ $EUID -ne 0 ]; then
     exitWithMsg 1 "Run this as root or with sudo privilege."
 fi
 
+export KUBECONFIG=/home/$SUDO_USER/.kube/config
+
 basedir=$(cd $(dirname $0) && pwd)
 
 k3dVersion="v5.0.1"
@@ -47,6 +49,7 @@ if [[ $(hash docker) -ne 0 ]]; then
     echo "Docker not found. Installing."
     sudo apt-get remove docker docker-engine docker.io containerd runc
     sudo apt install docker.io
+    echo "Docker installed."
 fi
 
 echo
@@ -62,11 +65,16 @@ echo "Checking K3d..."
 if [[ $(hash k3d) -ne 0 ]]; then
     echo "K3d not found. Installing."
     curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=$k3dVersion bash
+    echo "K3d installed."
 fi
 
 sleep 2
 
+echo
+echo "Creating cluster"
+echo
 k3d cluster create $clusterName --api-port 6550 --agents $nodeCount --k3s-arg "--disable=traefik@server:0" --k3s-arg "--disable=servicelb@server:0" --no-lb --wait
+echo "Cluster $clusterName created."
 
 echo "Checking kubectl..."
 if [[ $(hash kubectl) -ne 0 ]]; then
@@ -74,9 +82,10 @@ if [[ $(hash kubectl) -ne 0 ]]; then
     curl -LO https://dl.k8s.io/release/$kubectlVersion/bin/linux/amd64/kubectl
     chmod +x kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
+    echo "Kubectl installed."
 fi
 
-sleep 1
+sleep 2
 
 kubectl cluster-info
 
@@ -84,8 +93,14 @@ if [ $? -ne 0 ]; then
     exitWithMsg 1 "Failed to spinup cluster."
 fi
 
+echo
+echo "Deploying MetalLB loadbalancer."
+echo
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$metallbVersion/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$metallbVersion/manifests/metallb.yaml
+echo "Waiting for MetalLB to be ready. It may take 10 seconds or more."
+kubectl wait --timeout=150s --for=condition=ready pod -l app=metallb,component=controller -n metallb-system
+sleep 5
 
 sudo apt install jq -y
 cidr_block=$(docker network inspect k3d-$clusterName | jq '.[0].IPAM.Config[0].Subnet' | tr -d '"')
@@ -108,33 +123,39 @@ data:
       - $range
 EOF
 
+echo
+echo "Deploying Nginx Ingress Controller."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-$ingressControllerVersion/deploy/static/provider/aws/deploy.yaml
+echo "Waiting for Nginx Ingress controller to be ready. It may take 10 seconds or more."
+kubectl wait --timeout=150s  --for=condition=ready pod -l app.kubernetes.io/component=controller,app.kubernetes.io/instance=ingress-nginx -n ingress-nginx
 
 sleep 5
 
+echo "Getting Loadbalancer IP"
 externalIP=$(kubectl -n ingress-nginx get svc ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "LoadBalancer IP: $externalIP"
 
+echo
+echo "Deploying a sample app."
 kubectl apply -f https://raw.githubusercontent.com/navilg/spinup-k8s/master/sample-app.yaml
+echo "Waiting for sample application to be ready. It may take 10 seconds or more."
+kubectl wait --timeout=150s --for=condition=ready pod -l app=nginx -n sample-app
 
-kubectl wait --for=condition=ready pod -l app=nginx -n sample-app
-
-sleep 10
+sleep 5
 echo "Sample app is deployed."
 
 echo
 echo
-echo "---------------------------------------------------------"
-echo "---------------------------------------------------------"
+echo "---------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------"
 echo "Ingress Load Balancer: $externalIP"
-echo "Open sample app in browser: $externalIP/sampleap"
+echo "Open sample app in browser: http://$externalIP/sampleapp"
 echo "To stop this cluster (If running), run: k3d cluster stop $clusterName"
 echo "To start this cluster (If stopped), run: k3d cluster start $clusterName"
 echo "To delete this cluster, run: k3d cluster delete $clusterName"
 echo "To list all clusters, run: k3d cluster list"
-echo
-echo
-echo "---------------------------------------------------------"
-echo "---------------------------------------------------------"
+echo "---------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------"
 echo
 echo "|-- THANK YOU --|"
 echo
